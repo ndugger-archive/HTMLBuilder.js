025fd74e6d66e5c41248c55511d75f9ce7c0a628
Nick Dugger <email@nickdugger.com>
2014-12-15 11:26:43 -0600

    Update htmlbuilder.js

--- htmlbuilder.js
+++ htmlbuilder.js
@@ -1,30 +1,27 @@
 (function(HTMLBuilder) {
 	if (window.HTMLBuilder) console.error("Conflict: 'window.HTMLBuilder' already exists.");
-	else HTMLBuilder();
-})(function() {
-	function HTMLBuilder(childNodes, parentNode) {
-		if (typeof childNodes === "object") {
-			if (!parentNode) parentNode = document.createDocumentFragment();
-			if (!Array.isArray(childNodes)) childNodes = [childNodes];
-			for (var i = 0, count = childNodes.length; i < count; i++) {
-				if (childNodes[i].tagName || childNodes[i].nodeName) {
-					var child = document.createElement(childNodes[i].tagName || childNodes[i].nodeName);
-					delete childNodes[i].tagName || childNodes[i].nodeName;
-					for (var prop in childNodes[i]) {
-						if (prop === "classList") child.classList.add(childNodes[i][prop]);
-						else if (child.hasOwnProperty(prop) && prop !== "childNodes") child[prop] = childNodes[i][prop];
-					};
-					parentNode.appendChild(child);
-				} else {
-					parentNode.appendChild(document.createTextNode(childNodes[i].textContent));
-					var child = parentNode;
+	else window.HTMLBuilder = HTMLBuilder;
+})(function(childNodes, parentNode) {
+	if (typeof childNodes === "object") {
+		if (!parentNode) parentNode = document.createDocumentFragment();
+		if (!Array.isArray(childNodes)) childNodes = [childNodes];
+		for (var i = 0, count = childNodes.length; i < count; i++) {
+			if (childNodes[i].tagName || childNodes[i].nodeName) {
+				var child = document.createElement(childNodes[i].tagName || childNodes[i].nodeName);
+				delete childNodes[i].tagName || childNodes[i].nodeName;
+				for (var prop in childNodes[i]) {
+					if (prop === "classList") child.classList.add(childNodes[i][prop]);
+					else if (child.hasOwnProperty(prop) && prop !== "childNodes") child[prop] = childNodes[i][prop];
 				};
-				if (childNodes[i].childNodes) HTMLBuilder(childNodes[i].childNodes, child);
+				parentNode.appendChild(child);
+			} else {
+				parentNode.appendChild(document.createTextNode(childNodes[i].textContent));
+				var child = parentNode;
 			};
-			return parentNode;
-		} else {
-			console.error(new TypeError((typeof childNodes) + " is an invalid argument for childNodes."));
+			if (childNodes[i].childNodes) HTMLBuilder(childNodes[i].childNodes, child);
 		};
+		return parentNode;
+	} else {
+		console.error(new TypeError((typeof childNodes) + " is an invalid argument for childNodes."));
 	};
-	window.HTMLBuilder = HTMLBuilder;
 });
