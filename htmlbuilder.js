ab97be6247ea5064889464117c2750404a746d04
Nick Dugger <email@nickdugger.com>
2014-12-15 11:40:03 -0600

    Update htmlbuilder.js

--- htmlbuilder.js
+++ htmlbuilder.js
@@ -8,7 +8,8 @@
 		for (var i = 0, count = childNodes.length; i < count; i++) {
 			if (childNodes[i].tagName || childNodes[i].nodeName) {
 				var child = document.createElement(childNodes[i].tagName || childNodes[i].nodeName);
-				delete childNodes[i].tagName || childNodes[i].nodeName;
+				delete childNodes[i].tagName;
+				delete childNodes[i].nodeName;
 				for (var prop in childNodes[i]) {
 					if (prop === "classList") child.classList.add(childNodes[i][prop]);
 					else if (child.hasOwnProperty(prop) && prop !== "childNodes") child[prop] = childNodes[i][prop];
